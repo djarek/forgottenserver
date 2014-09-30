bc928954250c38e45da77800644737fee9c2c02b
Damian Jarek <damian.jarek93@gmail.com>
2014-09-30 04:11:49 +0200

    Removed unnecessary includes in protocolcast.cpp.

--- src/protocolcast.cpp
+++ src/protocolcast.cpp
@@ -19,32 +19,20 @@
 
 #include "otpch.h"
 
-#include <random>
-#include <boost/range/adaptor/reversed.hpp>
-
 #include "protocolcast.h"
 
 #include "networkmessage.h"
 #include "outputmessage.h"
-
-#include "items.h"
 
 #include "tile.h"
 #include "player.h"
 #include "chat.h"
 
 #include "configmanager.h"
-#include "actions.h"
+
 #include "game.h"
-#include "iologindata.h"
-#include "iomarket.h"
-#include "house.h"
-#include "waitlist.h"
-#include "quests.h"
-#include "mounts.h"
-#include "ban.h"
+
 #include "connection.h"
-#include "creatureevent.h"
 #include "scheduler.h"
 
 extern Game g_game;
